c0d61759af0e288d04c40305c46048d9a144f5d8
Maklakov Evgeniy <evgeniy.maklakov@gmail.com>
2020-03-26 18:25:47 +0600

    Реализован репозиторий для Feedback
    В репозитории данные через фабрику сохраняются в базу данных и отправляются на email.

---
+++ app/Factories/Feedback/FeedbackFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Factories\Feedback;
+
+
+class FeedbackFactory
+{
+	public $name;
+	public $phone;
+	public $message;
+}
--- app/Repositories/Feedback/FeedbackRepository.php
+++ app/Repositories/Feedback/FeedbackRepository.php
@@ -4,10 +4,18 @@
 namespace App\Repositories\Feedback;
 
 
+use App\Factories\Feedback\FeedbackFactory;
+
 class FeedbackRepository
 {
 	public static function save(array $data)
 	{
+		$factory = new FeedbackFactory();
+		$factory->name = $data['name'];
+		$factory->phone = $data['phone'];
+		$factory->message = $data['message'];
 
+		$factory->save('db');
+		$factory->save('email');
 	}
 }
